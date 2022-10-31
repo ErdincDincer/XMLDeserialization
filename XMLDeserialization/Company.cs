952103300384a1a68a5da4da7faf6630f42b4a16
ErdincDincer <erdincdincer@outlook.com>
2022-10-31 11:00:39 +0300

    Add project files.

---
+++ XMLDeserialization/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLDeserialization
+{
+    public class Company
+    {
+        public Company()
+        {
+            Addresses = new List<Address>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public List<Address> Addresses { get; set; }
+    }
+}
---
+++ XMLDeserialization/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Serialization;
+using System.Xml;
+
+namespace XMLDeserialization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Company));
+            //XmlReader'ın Create Methodunun içine dosya yolumuzu veya Url'i yazalım.
+            XmlReader reader = XmlReader.Create("https://www.siteadi.com/XMLVerilerim.xml");
+            Company company = (Company)xmlSerializer.Deserialize(reader);
+            reader.Close();
+        }
+    }
+}
